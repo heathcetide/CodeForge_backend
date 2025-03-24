5c7243c3f7ed11dc8355b75a0eaff0534ecbdc29
definition <2117350690@qq.com>
2025-03-24 10:59:11 +0800

    feat:更新临时秘钥功能

---
+++ src/main/java/com/cetide/codeforge/service/impl/FileServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cetide.codeforge.service.impl;
+
+
+import com.cetide.codeforge.service.FileService;
+import com.cetide.codeforge.util.JsonUtil;
+import com.cetide.codeforge.util.TemporaryUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Resource
+    TemporaryUtil temporaryUtil;
+
+    @Resource
+    JsonUtil jsonUtil;
+
+    public String createTemporaryKey() {
+        String key = temporaryUtil.getTemporaryCredentials();
+        String jsonKey = jsonUtil.convertToJson(key);
+        return jsonKey;
+    }
+}
