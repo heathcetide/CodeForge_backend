36779cacf82c2e17854cfed791b1c283770ea030
陈挺 <2148582258@qq.com>
2025-04-09 17:51:49 +0800

    feat: 删除爬虫模块

--- src/main/java/com/cetide/codeforge/mapper/WebsiteInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cetide.codeforge.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cetide.codeforge.model.entity.craw.WebsiteInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface WebsiteInfoMapper extends BaseMapper<WebsiteInfo> {
-} 
